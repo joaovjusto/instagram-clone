446376a727012e8907fa34e572e7771073578d43
João Justo <joaovjusto30@hotmail.com>
2019-06-17 12:46:18 -0300

    Primeiro Commit Frontend Web

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Header.css';
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+        <div className="header-content">
+            <img src="" alt="InstaClone"/>
+            <img src="" alt="Enviar Publicação"/>
+        </div>
+    </header>
+  );
+}
